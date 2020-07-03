7ebaf05182ca906d0cac2e80114a5abbc54b29fc
Ryan Duff <ryanduff613@gmail.com>
2020-07-03 11:12:09 -0700

    create loop in KegList.js

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,20 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Keg(){
-  const name = "Hefeveizen";
-  const brand = "widmer";
-  const price = "4.25";
-  const alcoholContent = "5%";
-
+function Keg(props){
+  
   return (
     <React.Fragment>
-      <h3>{ brand }</h3>
-      <h3>{ name }</h3>
-      <h3>{ price }</h3>
-      <h3>{ alcoholContent }</h3>
+      <h3>{ props.brand }</h3>
+      <h3>{ props.name }</h3>
+      <h3>{ props.price }</h3>
+      <h3>{ props.alcoholContent }</h3>
       <hr />
     </React.Fragment>
   );
+}
+
+Keg.propTypes = {
+  brand: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  alcoholContent: PropTypes.string.isRequired,
 }
 
 export default Keg;
--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,10 +1,34 @@
 import React from "react";
 import Keg from "./Keg";
 
+const masterKegList = [
+  {
+    brand: "widmer",
+    name: "hefeveizen",
+    price: "$5.00",
+    alcoholContent: "5%"
+  },
+  {
+    brand: "ninkasi",
+    name: "believer",
+    price: "$6.00",
+    alcoholContent: "8%"
+  }
+];
+
 
 function KegList(){
   return (
-    <Keg />
+    <React.Fragment>
+     <hr />
+     {masterKegList.map((keg, index) =>
+      <Keg brand={keg.brand}
+        name={keg.name}
+        price={keg.price}
+        alcoholContent={keg.alcoholContent}
+        key={index}/>
+     )}
+    </React.Fragment>
   );
 }
 
