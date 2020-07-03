5c91dd9b119a24115ddfc15e8de60aded2d9f515
Ryan Duff <ryanduff613@gmail.com>
2020-07-03 11:43:44 -0700

    create KegControl and NewKegForm componenets

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NewKegForm(props){
+  return (
+    <React.Fragment>
+      <h3>form goes here</h3>
+    </React.Fragment>
+  );
+}
+
+
+export default NewKegForm;
